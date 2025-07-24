ea53b6b46bb887d80ebc9695dea9f430be3535bf
dionlima1 <dion.lima@me.com>
2025-07-23 21:57:05 -0300

    index.ts

--- index.ts
+++ index.ts
@@ -11,15 +11,33 @@ import {
   RelayerAccount,
 } from '@generationsoftware/pt-v5-autotasks-library';
 
+async function getPoolTokenPriceUSD(): Promise<number | null> {
+  try {
+    const response = await nodeFetch(
+      'https://api.coingecko.com/api/v3/simple/price?ids=pooltogether&vs_currencies=usd'
+    );
+    const data = await response.json();
+    return data?.pooltogether?.usd || null;
+  } catch (e) {
+    console.error('Erro ao buscar preço POOL no CoinGecko:', e);
+    return null;
+  }
+}
 
-const main = async () =>{
+const main = async () => {
   const envVars: PrizeClaimerEnvVars = loadPrizeClaimerEnvVars();
   const provider: BaseProvider = getProvider(envVars);
 
   const relayerAccount: RelayerAccount = await instantiateRelayerAccount(
     provider,
-    envVars.CUSTOM_RELAYER_PRIVATE_KEY,
+    envVars.CUSTOM_RELAYER_PRIVATE_KEY
   );
+
+  const tokenPriceUSD = await getPoolTokenPriceUSD();
+  if (!tokenPriceUSD) {
+    console.error("Erro ao buscar o preço do token POOL. Abortando.");
+    return;
+  }
 
   const config: PrizeClaimerConfig = {
     ...relayerAccount,
@@ -27,13 +45,13 @@ const main = async () =>{
     chainId: envVars.CHAIN_ID,
     rewardRecipient: envVars.REWARD_RECIPIENT,
     minProfitThresholdUsd: Number(envVars.MIN_PROFIT_THRESHOLD_USD),
-    covalentApiKey: envVars.COVALENT_API_KEY,
+    covalentApiKey: "", // desativado
     contractJsonUrl: envVars.CONTRACT_JSON_URL,
     subgraphUrl: envVars.SUBGRAPH_URL,
   };
 
   const contracts: ContractsBlob = await downloadContractsBlob(config.contractJsonUrl, nodeFetch);
   await runPrizeClaimer(contracts, config);
-}
+};
 
-main() 
+main();
